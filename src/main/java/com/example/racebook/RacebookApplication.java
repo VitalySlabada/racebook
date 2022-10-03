9f3f7ad7cb7a699f6ca6faf80fd39f42e5337894
VitalySlabada <vitaly.slabada@gmail.com>
2022-10-02 20:25:31 -0500

    Initial commit

---
+++ src/main/java/com/example/racebook/RacebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.racebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/racebook/RacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.racebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
