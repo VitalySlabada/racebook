72f0901adbf0634f6db0f3baf606a210e766a2a9
VitalySlabada <vitaly.slabada@gmail.com>
2022-10-16 21:07:11 -0500

    Login Controller

--- src/test/java/com/example/racebook/RacebookApplicationTests.java
+++ src/test/java/com/example/racebook/RacebookApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.racebook;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RacebookApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.racebook;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RacebookApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
